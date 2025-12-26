977d3c1c6e400ef0934000c50a78fc2ace58bed1
Manuel Ruiz Redondo <manuelruizredondo@gmail.com>
2025-12-26 12:20:54 +0100

    Remove unused files and update project dependencies in package.json; change agenda date in index.njk; update site metadata in site.webmanifest.

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,9 @@
 import 'regenerator-runtime/runtime';
 import { gsap } from 'gsap';
-import Rellax from 'rellax';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Swiper, { Navigation } from 'swiper';
-Swiper.use([Navigation]);
-
 
 //init zone
 gsap.registerPlugin(ScrollTrigger);
-var rellax = new Rellax('.rellax');
-console.log(rellax, mySwiper);
-
 
 document.body.onscroll = function() {
   if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
@@ -19,31 +12,6 @@ document.body.onscroll = function() {
     document.body.classList.remove("scrolled");
   }
 };
-
-
-
-
-
-  var menu = ['Slide 1', 'Slide 2', 'Slide 3']
-  var mySwiper = new Swiper ('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (menu[index]) + '</span>';
-          },
-      },
-  
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    })
-  
 
 
 
